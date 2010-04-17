f99c58c452353742d547469d7aab29022c9af8ed
zhangkaizhao <cool@Fedora.localdomain>
2010-04-17 15:16:25 +0800

    version 0.1 for appengine

--- blog.py
+++ blog.py
@@ -12,12 +12,16 @@ from google.appengine.ext import db
 
 from lib import htmlconverter
 
+# cookie_secret:
+#import hashlib
+#cookie_secret = hashlib.sha1("zhangkaizhao").hexdigest()
+
 AUTHOR = u'metalman' # or get administrator's nickname from google account
                      # in a request handler: self.current_user.nickname()
-POSTS_PER_PAGE = 2
-POSTS_PER_ARCHIVE = 3
+POSTS_PER_PAGE = 10
+POSTS_PER_ARCHIVE = 20
 POSTS_IN_FEED = 10
-COMMENTS_PER_PAGE = 2
+COMMENTS_PER_PAGE = 40
 
 class BlogIndex(db.Model):
     """ index model for entry model to make faster and for easy paging """
@@ -34,6 +38,7 @@ class BlogEntry(db.Model):
     title = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     html = db.TextProperty(required=True)
+    tags = db.StringListProperty()
     published = db.DateTimeProperty(auto_now_add=True)
     updated = db.DateTimeProperty(auto_now=True)
 
@@ -46,6 +51,11 @@ class Comment(db.Model):
     content = db.TextProperty(required=True)
     published = db.DateTimeProperty(auto_now_add=True)
 
+class Tag(db.Model):
+    """ tag model. no need index because few """
+    name = db.StringProperty(required=True)
+    count = db.IntegerProperty(default=0)   # number of posts
+
 def get_entry(index):
     """ get an entry by index """
     # give another param AUTHOR if in multi author
@@ -53,26 +63,49 @@ def get_entry(index):
         parent=db.Key.from_path('BlogIndex', AUTHOR))
     return entry
 
-def get_entries(start_index, batch_size=10):
+def get_entries(start_index, batch_size=10, **kwargs):
     """ get entries by start index and batch size. """
     extra = None
+
+    if kwargs and kwargs.has_key('tag'):
+        tag = kwargs.pop('tag')
+    else:
+        tag = None
+
     if start_index is None:
-        entries = BlogEntry.gql(
-            'ORDER BY index DESC').fetch(
-            int(batch_size) + 1)
+        if tag is None:
+            entries = BlogEntry.gql(
+                'ORDER BY index DESC').fetch(
+                int(batch_size) + 1)
+        else:
+            entries = BlogEntry.gql(
+                'WHERE tags = :1 ORDER BY index DESC',
+                tag).fetch(
+                int(batch_size) + 1)
     else:
         start_index = int(start_index)
-        entries = BlogEntry.gql(
-            'WHERE index <= :1 ORDER BY index DESC',
-            int(start_index)).fetch(int(batch_size) + 1)
+        if tag is None:
+            entries = BlogEntry.gql(
+                'WHERE index <= :1 ORDER BY index DESC',
+                int(start_index)).fetch(int(batch_size) + 1)
+        else:
+            entries = BlogEntry.gql(
+                'WHERE index <= :1 AND tags = :2 ORDER BY index DESC',
+                int(start_index), tag).fetch(int(batch_size) + 1)
     if len(entries) > int(batch_size):
         extra = entries[-1]
         entries = entries[:int(batch_size)]
     return entries, extra
 
-def post_entry(title, content, html):
+def post_entry(title, content, html, tags):
     """ create an entry """
     # give another param AUTHOR if in multi author
+    if tags:
+        for tag in tags:
+            old_tag = Tag.gql("WHERE name = :1", tag).get()
+            new_tag = old_tag and old_tag or Tag(name=tag)
+            new_tag.count += 1
+            new_tag.put()
     def txn():
         blog_index = BlogIndex.get_by_key_name(AUTHOR)
         if blog_index is None:
@@ -88,18 +121,34 @@ def post_entry(title, content, html):
             title=title,
             content=content,
             html=html,
+            tags=tags,
         )
         new_entry.put()
         return new_index
     return db.run_in_transaction(txn)
 
-def update_entry(index, title, content, html):
+def update_entry(index, title, content, html, tags):
     """ update an entry by index. """
     entry = get_entry(index)
+    entry_tags = entry.tags
+    removed_entry_tags = set(entry_tags) - set(tags)
+    added_entry_tags = set(tags) - set(entry_tags)
+    keeped_entry_tags = set(tags).intersection(set(entry_tags))
+    for tag in tags:
+        old_tag = Tag.gql("WHERE name = :1", tag).get()
+        new_tag = old_tag and old_tag or Tag(name=tag)
+        if tag in added_entry_tags:
+            new_tag.count += 1
+        new_tag.put()
+    for tag in removed_entry_tags:
+        old_tag = Tag.gql("WHERE name = :1", tag).get()
+        old_tag.count -= 1
+        old_tag.put()
     def txn():
         entry.title = title
         entry.content = content
         entry.html = html
+        entry.tags = tags
         entry.put()
     db.run_in_transaction(txn)
 
@@ -147,6 +196,11 @@ def post_comment(entry_index, author, website, content):
         new_comment.put()
     db.run_in_transaction(txn)
 
+def get_tags():
+    """ number of tags is few, no more than 1000. """
+    tags = db.Query(Tag)
+    return tags and tags or []
+
 def administrator(method):
     """Decorate with this method to restrict to site admins."""
     @functools.wraps(method)
@@ -181,7 +235,6 @@ class BaseHandler(tornado.web.RequestHandler):
         return tornado.web.RequestHandler.render_string(
             self, template_name, users=users, **kwargs)
 
-
 class HomeHandler(BaseHandler):
     def get(self):
         start = self.get_argument("start", None)
@@ -207,6 +260,12 @@ class ArchiveHandler(BaseHandler):
         entries, extra = get_entries(start, POSTS_PER_ARCHIVE)
         self.render("archive.html", entries=entries, extra=extra)
 
+class TagHandler(BaseHandler):
+    def get(self, tag):
+        start = self.get_argument("start", None)
+        entries, extra = get_entries(start, POSTS_PER_PAGE, tag=tag)
+        self.render("home.html", entries=entries, extra=extra)
+
 
 class FeedHandler(BaseHandler):
     def get(self):
@@ -221,11 +280,15 @@ class ComposeHandler(BaseHandler):
     def get(self):
         index = self.get_argument("index", None)
         entry = get_entry(index) if index else None
-        self.render("compose.html", entry=entry)
+        tags = get_tags()
+        tags = tags and " ".join([t.name for t in tags]) or ""
+        self.render("compose.html", entry=entry, tags=tags)
 
     @administrator
     def post(self):
         index = self.get_argument("index", None)
+        tags = self.get_argument("tags", None)
+        tags = tags and [t for t in set(tags.lower().split())] or []
         if index:
             # edit an entry
             update_entry(
@@ -233,6 +296,7 @@ class ComposeHandler(BaseHandler):
                 title=self.get_argument("title"),
                 content=self.get_argument("content"),
                 html=markdown.markdown(self.get_argument("content")),
+                tags=tags,
             )
         else:
             # create an entry
@@ -240,22 +304,28 @@ class ComposeHandler(BaseHandler):
                 title=self.get_argument("title"),
                 content=self.get_argument("content"),
                 html=markdown.markdown(self.get_argument("content")),
+                tags=tags,
             )
         self.redirect("/entry/" + str(index))
 
 class CommentHandler(BaseHandler):
     def post(self):
         entry_index = self.get_argument("entry_index")
+        website=self.get_argument("website", "")
+        if website and not website.startswith("http://"):
+            website = u"http://" + website
         content=htmlconverter.webtext2html(self.get_argument("content"))
         if not isinstance(content, unicode):
             content = unicode(content)
+        # TODO AJAX response
+        if len(content) > 1000: raise tornado.web.HTTPError(404)
         post_comment(
             entry_index=entry_index,
             author=self.get_argument("author"),
-            website=self.get_argument("website", ""),
+            website=website,
             content=content,
         )
-        self.redirect("/entry/" + str(entry_index))
+        self.redirect("/entry/" + str(entry_index) + '#comment')
 
 class AboutHandler(BaseHandler):
     def get(self):
@@ -282,26 +352,38 @@ class CommentModule(tornado.web.UIModule):
     def render(self, comment):
         return self.render_string("modules/comment.html", comment=comment)
 
+class TagsModule(tornado.web.UIModule):
+    def render(self):
+        return self.render_string("modules/tags.html", tags=get_tags())
 
-settings = {
-    "blog_title": u"metalman's blog",
-    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-    "ui_modules": {"Entry": EntryModule, "Comment": CommentModule},
-    "xsrf_cookies": True,
-}
-application = tornado.wsgi.WSGIApplication([
-    (r"/", HomeHandler),
-    (r"/archive", ArchiveHandler),
-    (r"/feed", FeedHandler),
-    (r"/entry/([^/]+)", EntryHandler),
-    (r"/compose", ComposeHandler),
-    (r"/comment", CommentHandler),
-    (r"/about", AboutHandler),
-], **settings)
+
+class WSGIApplication(tornado.wsgi.WSGIApplication):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+            (r"/archive", ArchiveHandler),
+            (r"/tag/([^/]+)", TagHandler),
+            (r"/feed", FeedHandler),
+            (r"/entry/([^/]+)", EntryHandler),
+            (r"/compose", ComposeHandler),
+            (r"/comment", CommentHandler),
+            (r"/about", AboutHandler),
+        ]
+
+        settings = dict(
+            blog_title=u"metalman's blog",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            ui_modules={"Entry": EntryModule,
+                        "Comment": CommentModule,
+                        "Tags": TagsModule,},
+            xsrf_cookies=True,
+            cookie_secret="5e4b388cec80152c7bb967fb5b63ba62750a13a0",
+        )
+        tornado.wsgi.WSGIApplication.__init__(self, handlers, **settings)
 
 
 def main():
-    wsgiref.handlers.CGIHandler().run(application)
+    wsgiref.handlers.CGIHandler().run(WSGIApplication())
 
 
 if __name__ == "__main__":
--- templates/about.md
+++ templates/about.md
@@ -1,6 +1,15 @@
 About this blog
 ---------------
 
+### Why this blog
+
+Because I can't use [Blogger](http://halfcool-devel.blogspot.com/)
+and [Google site](http://sites.google.com/site/halfcool/)
+directly for a long time. I'm just a programmer and I do all about
+programming. :-)
+
+### How this blog come out
+
 This blog is modified from the appengine demo of
 [Tornado](http://github.com/facebook/tornado).
 And the Models Index and paging code is modified
@@ -8,29 +17,51 @@ from [董洵](http://www.alexdong.com/)'s speech
 **深入云计算平台之Google App Engine**
 on 2008 SD2.0 Conference.
 
-About author
-------------
+About the author
+----------------
 
-My name is Kaizhao Zhang.
+### Overview
+
+My name is Kaizhao Zhang. I'm Chaoshanese.
+Graduated in Jul,2005 from SCUT.
+
 I'm deep in learning computer technologies.
-And I use Fedora, Debian and Cygwin for study and working.
+I use Fedora, Debian and Cygwin for study and working now.
 I'm interested in C, Python, etc. And I'm interested in both
 web development and game development.
+Now this guy is looking for a good job and looking for his
+pretty woman. :-)
 
-Here is my gmail: [zhangkaizhao AT gmail DOT com](mailto:zhangkaizhao AT gmail DOT com)
+### Gmail
 
-My other links
---------------
+In [Python](http://www.python.org/):
 
-  * http://halfcool-devel.blogspot.com/
-  * http://hi.baidu.com/whycrying
-  * http://sites.google.com/site/halfcool/
-  * http://github.com/metalman
+    "zhangkaizhao AT gmail DOT com".replace(' AT ', '@').replace(' DOT ', '.')
+
+### Links
+
   * twitter: [@halfcool](http://twitter.com/halfcool)
-  * douban: [@halfcool](http://www.douban.com/people/halfcool)
+  * douban: [@halfcool](http://www.douban.com/people/halfcool/)
   * zeuux: [@halfcool](http://www.zeuux.com/friend/user/halfcool/)
+  * [http://halfcool-devel.blogspot.com/](http://halfcool-devel.blogspot.com/)
+  * [http://hi.baidu.com/whycrying](http://hi.baidu.com/whycrying)
+  * [http://sites.google.com/site/halfcool/](http://sites.google.com/site/halfcool/)
+  * [http://github.com/metalman](http://github.com/metalman)
 
-Technologies learning now
--------------------------
+Observing
+---------
 
-  * Redis
+  * Languages: [Go](http://golang.org/), [Lua](http://www.lua.org/)
+  * NoSQL: [Redis](http://code.google.com/p/redis), [MongoDB](http://www.mongodb.org/)
+  * Python Web Servers: [Tornado](http://github.com/facebook/tornado), [gevent](http://bitbucket.org/denis/gevent/)
+
+Some links
+---------------
+
+  * [Nicholas](http://nichol.as/)
+  * [Tim](http://timyang.net/)
+  * [cloudwu](http://blog.codingnow.com/)
+  * [LinuxTOY](http://linuxtoy.org/)
+
+--
+Last updated on Apr,17,2010
